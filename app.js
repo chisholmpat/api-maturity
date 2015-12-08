cc5338334b3c685550b6844c2723dd45ae652a54
Gregory Hatt <groggle@yandex.com>
2015-12-08 14:26:37 -0400

    Add CFEnv Port Configuration and remove error handling methods.

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cfenv = require('cfenv');
 
+// route file includes
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -33,36 +34,12 @@ app.get('/', function(req, res) {
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// hosting config
+var appEnv = cfenv.getAppEnv();
+var port = appEnv.port || '3000';
+// start server on the specified port and binding host
+app.listen(appEnv.port , function() {
+    console.log("server starting on " + appEnv.url);
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
