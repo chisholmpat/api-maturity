1cee7127e3b17ba69f96b85adede51ca8e5b81aa
Gregory Hatt <groggle@yandex.com>
2015-12-09 19:37:32 -0400

    Add some sample records and modified questionController/Server.

--- public/js/questionnaire/questionnaireController.js
+++ public/js/questionnaire/questionnaireController.js
@@ -2,28 +2,19 @@
     var module = angular.module('questionnaireModule', ['questionnaireServiceModule'])
 
 module.controller('QuestionnaireController', ['$scope', 'QuestionStore', function($scope, QuestionStore) {
-            $scope.questions = QuestionStore.allQuestionConn.query();
-
-        $scope.responses = [{
-            id : 1,
-            text :'Don\'t do it'
-         },
-         {
-            id : 2,
-            text : 'Planned'
-         },
-         {
-            id: 3,
-            text : 'Rose Smells.'
-         }
-         ];
-
+	
+	// Get all questions from the backend.
+	$scope.questions = QuestionStore.allQuestionConn.query();
+        // These are just hard coded but will have to be generated from the database.
+	$scope.responses = [{ id : 1, text :'Don\'t do it'},{id : 2, text : 'Planned'},{id: 3, text : 'Response 3.'}];
+	// Debug printing
         $scope.generateScore = function(questions) {                
                 for(i = 0; i < questions.length; i++)
                     console.log(questions[i].response);
         }
     }]);
 
+// Backend does not currently support this. Will implement tomorrow.
 module.controller('QuestionController', ['$scope', 'QuestionStore', '$routeParams', function($scope, QuestionStore, $routeParams){
                 console.log($routeParams.clientid);
 }]);
