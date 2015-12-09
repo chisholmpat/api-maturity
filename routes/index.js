294e993ed89d44f3c6b5fefef430d8be7e3bc9f8
Gregory Hatt <groggle@yandex.com>
2015-12-09 15:13:26 -0400

    Pre-merge commit.

--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,26 @@ module.exports = function(app) {
       res.sendFile('__dirname' + '/public/index.html');
     });
 
-    app.get('/questions', function(req, res) {
+    app.get('/greg-questions', function(req, res) {
 
-
-         var responses = [{
-            id : 1,
-            text :'Don\'t do it'
-         },
-        {
-            id : 2,
-            text : 'Planned'
-        },
-        {
-            id: 3,
-            text : 'Rose Smells.'
-        }
-         ];        
 
          var questions = [{ 
             id : 1, 
             text : 'Capture Business and Technical API measurements or metrics',
             form_id : 1,
-            responses : responses,
-            answer : ''
+            response : ""
         }, 
          {
             id : 2,
             text : 'Do you have Categories of Business and Technical API measurements or metrics',
             form_id : 1,
-            responses : responses,
-            answer : ''
+            response : ""
          },
         {
             id : 3,
             text : 'Capture KPIs that determine the business value of applications',
             form_id : 1,
-            responses : responses,
-            answer : ''
+            response : "",
         }
         ];
        
