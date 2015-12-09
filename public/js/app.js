fd37eea0f194597d67e97ad775d60e103a490f16
Gregory Hatt <groggle@yandex.com>
2015-12-08 20:39:47 -0400

    Added example of routing requests in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-angular.module('app', []);
+var myApp = angular.module('app', ['ngRoute']);
+
+myApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'views/welcome/welcome.html',
+        controller: 'HomePageController'
+      }).
+      when('/questionnaire', {
+        templateUrl: 'views/questionnaire/questionnaire.html',
+        controller: 'QuestionnaireController'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+  }]);
+
+myApp.controller('HomePageController', function($scope) {
+    $scope.message = 'This is the welcome page.';
+});
+
+myApp.controller('QuestionnaireController', function($scope) {
+        $scope.message = 'This is the questionnaire page.';
+});
+
+
+
