6cae4fcad55258cd1376f974f7f62c767a9fa55e
Gregory Hatt <groggle@yandex.com>
2015-12-09 19:27:22 -0400

    Fixed query in questions.js and query_.js

--- querying/questionsQuerying.js
+++ querying/questionsQuerying.js
@@ -3,10 +3,10 @@ var file = "./db/api_maturity.sqlite";
 var sqlite3 = require('sqlite3').verbose();
 var dbAdapter = new sqlite3.Database(file);
 
-exports.getAllQuestionsClientForm = function (req, res, err_string, results_array, form_id, client_id, question_category, callBack) {
+exports.getAllQuestionsClientForm = function (req, res, err_string, results_array, form_id, client_id, question_category_id, callBack) {
   dbAdapter.serialize(function(){
     //Perform SELECT Operation
-    dbAdapter.all("SELECT Question.text, ClientQuestionResponse.weight FROM ClientQuestionResponse INNER JOIN Question  ON ClientQuestionResponse.question_id=Question.id WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ? AND Question.category = ?; ", form_id, client_id, question_category,
+    dbAdapter.all("SELECT Question.text, ClientQuestionResponse.weight FROM ClientQuestionResponse INNER JOIN Question ON ClientQuestionResponse.question_id=Question.id WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ? AND Question.category_id = ?; ", form_id, client_id, question_category_id,
       function(err,rows){
         //query result dumped as an array into results_array;
         err_string = err;
--- routes/questions.js
+++ routes/questions.js
@@ -1,14 +1,19 @@
 var questionsQuerying = require('../querying/questionsQuerying.js');
 
+
+// Call back function from the database call. Used to send either the results 
+// to the response or to send an error string to the response.
 function getAllQuestionsClientFormCallBack(req, res, err_string, results_array){
-    console.log(err_string);
-    console.log(results_array);
-    if(!err_string){
-      res.send(results_array);
-    }
-    else{
-      res.send(err_string);
-    }
+	
+	console.log(err_string);
+	console.log(results_array);
+	
+	if(!err_string){
+		res.send(results_array);
+	}
+	else{
+		res.send(err_string);
+	}
 }
 
 module.exports = function(app) {
@@ -21,16 +26,16 @@ module.exports = function(app) {
     }));
 
     app.get('/questions', function(req, res, next) {
+    
       var err_string;
       var results_array;
       var form_id = req.body.form_id || 1;
       var client_id = req.body.client_id || 1;
-      var question_category = req.body.question_category || "QA";
-
-      questionsQuerying.getAllQuestionsClientForm(req, res, err_string, results_array, form_id, client_id, question_category, getAllQuestionsClientFormCallBack);
+      var question_category_id = req.body.question_category || 1;
+      questionsQuerying.getAllQuestionsClientForm(
+		req, res, err_string, results_array, 
+		form_id, client_id, question_category_id, 
+		getAllQuestionsClientFormCallBack);
     });
 
-    // app.get('/questions/:form_id:category:client_id', function(req, res) {
-    //
-    // });
 }
