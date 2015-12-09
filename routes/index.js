f0f99dde66366d4bb1277cc7a0ea54f1503ba6fc
Gregory Hatt <groggle@yandex.com>
2015-12-09 12:11:53 -0400

    Add working form example.

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,39 @@ module.exports = function(app) {
     });
 
     app.get('/questions', function(req, res) {
-              
+
+
+         var responses = [{
+            id : 1,
+            text :'Don\'t do it'
+         },
+        {
+            id : 2,
+            text : 'Planned'
+        },
+        {
+            id: 3,
+            text : 'Rose Smells.'
+        }
+         ];        
+
          var questions = [{ 
             id : 1, 
             text : 'Capture Business and Technical API measurements or metrics',
-            form_id : 1
+            form_id : 1,
+            response : responses
         }, 
          {
             id : 2,
             text : 'Do you have Categories of Business and Technical API measurements or metrics',
-            form_id : 1
+            form_id : 1,
+            response : responses
          },
         {
             id : 3,
             text : 'Capture KPIs that determine the business value of applications',
-            form_id : 1
+            form_id : 1,
+            response : responses
         }
         ];
        
