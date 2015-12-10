e8a67155d6d282ce11f41066c22b16978c259204
Gregory Hatt <groggle@yandex.com>
2015-12-10 09:40:15 -0400

    Add example of getting req params.

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,11 @@ module.exports = function(app) {
       res.sendFile('__dirname' + '/public/index.html');
     });
 
-    app.get('/greg-questions', function(req, res) {
+    app.get('/greg-questions/:someNumber', function(req, res) {
 
-
-         var questions = [{ 
-            id : 1, 
-            text : 'Capture Business and Technical API measurements or metrics',
-            form_id : 1,
-            response : ""
-        }, 
-         {
-            id : 2,
-            text : 'Do you have Categories of Business and Technical API measurements or metrics',
-            form_id : 1,
-            response : ""
-         },
-        {
-            id : 3,
-            text : 'Capture KPIs that determine the business value of applications',
-            form_id : 1,
-            response : "",
-        }
-        ];
+       var someNumber = req.params.someNumber; 
        
-            res.send(questions);
+            res.send(someNumber);
     });
 }
 
