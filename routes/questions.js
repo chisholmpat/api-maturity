b24850fbea11106f20c67b4b5adf3108e51be96e
rosejp <rose@ca.ibm.com>
2015-12-10 10:34:30 -0400

    Latest pull

--- routes/questions.js
+++ routes/questions.js
@@ -1,13 +1,13 @@
 var questionsQuerying = require('../querying/questionsQuerying.js');
 
 
-// Call back function from the database call. Used to send either the results 
+// Call back function from the database call. Used to send either the results
 // to the response or to send an error string to the response.
 function queryCallback(req, res, err_string, results_array){
-	
+
 	console.log(err_string);
 	console.log(results_array);
-	
+
 	if(!err_string){
 		res.send(results_array);
 	}
@@ -27,27 +27,28 @@ module.exports = function(app) {
 
 
     app.get('/questions', function(req, res, next) {
-    
+
       var err_string;
       var results_array;
       var form_id = req.body.form_id || 1;
       var client_id = req.body.client_id || 1;
       var question_category_id = req.body.question_category || 1;
       questionsQuerying.getAllQuestionsClientForm(
-		req, res, err_string, results_array, 
-		form_id, client_id, question_category_id, 
-		queryCallback);
+    		req, res, err_string, results_array,
+    		form_id, client_id, question_category_id,
+    		queryCallback
+      );
     });
-    
+
       app.get('/responses', function(req, res, next) {
-    
+
       var err_string;
       var results_array;
       var response_category_id = req.body.response_category || 1;
       questionsQuerying.getAllResponses(
-		req, res, err_string, results_array, 
-		response_category_id, 
-		queryCallback);
-    });
+    		req, res, err_string, results_array,
+    		response_category_id,
+    		queryCallback);
+      });
 
 }
