5d5936828088e9d3165bad4b69ab760125ed3d8d
Gregory Hatt <groggle@yandex.com>
2015-12-10 15:45:05 -0400

    Filtering of question working.

--- public/js/results/resultsController.js
+++ public/js/results/resultsController.js
@@ -1,13 +1,10 @@
-(function(){
-
-var module = angular.module('resultsModule', ['resultsServiceModule']);
-
-module.controller('ResultsController', ['$scope', '$routeParams', 'ResultStore', function($scope, $routeParams, ResultStore) {
-
-	$scope.results = ResultStore.scoreConn.query({client_id: $routeParams.client_id, form_id : $routeParams.form_id});
-
-	console.log($scope.results);
-
-}]);
-
+(function() {
+	  // Retrieve all information required to generate score.
+    var module = angular.module('resultsModule', ['resultsServiceModule']);
+    module.controller('ResultsController', ['$scope', '$routeParams', 'ResultStore', function($scope, $routeParams, ResultStore) {
+				$scope.results = ResultStore.scoreConn.query({
+            client_id: $routeParams.client_id,
+            form_id: $routeParams.form_id
+        });			
+    }]);
 })();
--- public/js/results/resultsService.js
+++ public/js/results/resultsService.js
@@ -1,8 +1,8 @@
-( function() {
-
-var module = angular.module('resultsServiceModule', ['ngResource']);
+(function() {
+    // Provides access to the end point which produces all values required to
+    // geneate the score to be provided on the front end.
+    var module = angular.module('resultsServiceModule', ['ngResource']);
     module.service('ResultStore', ['$http', '$resource', function($http, $resource) {
-       this.scoreConn = $resource('/score/:client_id/:form_id');
+        this.scoreConn = $resource('/score/:client_id/:form_id');
     }]);
-
 })();
