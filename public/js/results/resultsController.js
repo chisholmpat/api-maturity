d2e663c0dcb8c0f80927292af80d87f8fdf3ffaf
rosejp <rose@ca.ibm.com>
2015-12-12 01:23:38 -0400

    fixing merge conflicts

--- public/js/results/resultsController.js
+++ public/js/results/resultsController.js
@@ -4,6 +4,7 @@
 	  // Retrieve all information required to generate score.
 
     function calculateAllScores(response_value, question_weight){
+
       if( (response_value==1||response_value==2) && (question_weight==1||question_weight==2) ){
         return response_value-1;
       }
@@ -14,16 +15,12 @@
 
     function getGraphScores(results){
 
-      array_length = results.length;
-      console.log("graphScores");
+      var array_length = results.length;
 
       //Calculate Scores
       for(i=0; i<array_length; i++){
           results[i].score = calculateAllScores(results[i].value, results[i].weight);
-          console.log(results[i]);
       }
-      //Calcualte the final graph value based on the average of each group
-      console.log("forAveragesArray");
     }
 
 
@@ -38,6 +35,7 @@
         var current_array;
 
         //Get the data
+        console.log("Hi");
 				$scope.results = ResultStore.scoreConn.query({
             client_id: $routeParams.client_id,
             form_id: $routeParams.form_id,
@@ -45,6 +43,5 @@
             getGraphScores($scope.results);
           }
         );
-
     }]);
 })();
--- routes/questions.js
+++ routes/questions.js
@@ -6,7 +6,7 @@ function queryCallback(req, res, err_string, results_array, connection){
 		res.send(results_array);
 	}
 	else{
-	
+
 		res.send(err_string);
 	}
 }
@@ -17,12 +17,12 @@ module.exports = function(app) {
 	var db = require('../db/db.js');
 	var connection = db.getConnection();
     var bodyParser = require('body-parser');
-	
+
     app.use( bodyParser.json() );       // to support JSON-encoded bodies
     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
       extended: true
     }));
-    
+
 	// Return all questions by client and form id
 	app.get('/questions/:client_id/:form_id', function(req, res) {
 
@@ -33,7 +33,7 @@ module.exports = function(app) {
 
 		var query = connection.query('SELECT ClientQuestionResponse.client_id, Question.id, Question.text, Question.category_id, ClientQuestionResponse.response_id, ClientQuestionResponse.weight\
 			FROM ClientQuestionResponse INNER JOIN Question ON ClientQuestionResponse.question_id=Question.id\
-			WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ?; ', 
+			WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ?; ',
 			[form_id, client_id], function (error, results) {
 				queryCallback(req, res, error, results, connection);
 		});
@@ -46,7 +46,7 @@ module.exports = function(app) {
 		var results_array;
 		var form_id = req.params.form_id || 1;
 		var client_id = req.params.client_id || 1;
-		
+
 		var query = connection.query('SELECT Response.response, Response.value, Question.text, Question.id, ClientQuestionResponse.response_id,\
 			ClientQuestionResponse.weight, Question.category_id\
 			FROM ClientQuestionResponse\
@@ -54,19 +54,19 @@ module.exports = function(app) {
 			LEFT  JOIN Response ON ClientQuestionResponse.response_id=Response.id \
 			WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ?', [form_id, client_id],  function (error, results) {
 					queryCallback(req, res, error, results, connection);
-			});		
+			});
     });
 
 	// Return all responses
     app.get('/responses', function(req, res, next) {
-	    
+
 		var err_string;
 	    var results_array;
-	
+
 		var query = connection.query("SELECT * FROM Response",  function (error, results) {
 					queryCallback(req, res, error, results, connection);
-		});	
-		
+		});
+
 	  });
 
 }
