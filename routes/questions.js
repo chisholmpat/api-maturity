fbf6b1d3781818093012af1486ce9bd56b92919d
rosejp <rose@ca.ibm.com>
2015-12-12 02:07:25 -0400

    query updated, data accessible to controller

--- routes/questions.js
+++ routes/questions.js
@@ -6,7 +6,7 @@ function queryCallback(req, res, err_string, results_array, connection){
 		res.send(results_array);
 	}
 	else{
-	
+
 		res.send(err_string);
 	}
 }
@@ -17,12 +17,12 @@ module.exports = function(app) {
 	var db = require('../db/db.js');
 	var connection = db.getConnection();
     var bodyParser = require('body-parser');
-	
+
     app.use( bodyParser.json() );       // to support JSON-encoded bodies
     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
       extended: true
     }));
-    
+
 	// Return all questions by client and form id
 	app.get('/questions/:client_id/:form_id', function(req, res) {
 
@@ -33,7 +33,7 @@ module.exports = function(app) {
 
 		var query = connection.query('SELECT ClientQuestionResponse.client_id, Question.id, Question.text, Question.category_id, ClientQuestionResponse.response_id, ClientQuestionResponse.weight\
 			FROM ClientQuestionResponse INNER JOIN Question ON ClientQuestionResponse.question_id=Question.id\
-			WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ?; ', 
+			WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ?; ',
 			[form_id, client_id], function (error, results) {
 				queryCallback(req, res, error, results, connection);
 		});
@@ -46,27 +46,28 @@ module.exports = function(app) {
 		var results_array;
 		var form_id = req.params.form_id || 1;
 		var client_id = req.params.client_id || 1;
-		
+
 		var query = connection.query('SELECT Response.response, Response.value, Question.text, Question.id, ClientQuestionResponse.response_id,\
-			ClientQuestionResponse.weight, Question.category_id\
+			ClientQuestionResponse.weight, Question.category_id, Question.group_id, Grouping.name\
 			FROM ClientQuestionResponse\
 			INNER JOIN Question ON ClientQuestionResponse.question_id=Question.id \
-			LEFT  JOIN Response ON ClientQuestionResponse.response_id=Response.id \
+			INNER JOIN Response ON ClientQuestionResponse.response_id=Response.id \
+			INNER JOIN Grouping ON Question.group_id=Grouping.id \
 			WHERE Question.form_id = ? AND ClientQuestionResponse.client_id = ?', [form_id, client_id],  function (error, results) {
 					queryCallback(req, res, error, results, connection);
-			});		
+			});
     });
 
 	// Return all responses
     app.get('/responses', function(req, res, next) {
-	    
+
 		var err_string;
 	    var results_array;
-	
+
 		var query = connection.query("SELECT * FROM Response",  function (error, results) {
 					queryCallback(req, res, error, results, connection);
-		});	
-		
+		});
+
 	  });
 
 }
