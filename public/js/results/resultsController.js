da01b7811885cb36b4c4302c9ad81365e2557600
Gregory Hatt <groggle@yandex.com>
2015-12-12 15:21:36 -0400

    Add scores.

--- public/js/results/resultsController.js
+++ public/js/results/resultsController.js
@@ -5,6 +5,12 @@
 				$scope.results = ResultStore.scoreConn.query({
             client_id: $routeParams.client_id,
             form_id: $routeParams.form_id
-        });
+        }, function() {
+                    results = $scope.results;
+                    for(i=0; i<results.length; i++){
+                        results[i].score = results[i].value == 1 || results[i].value ==2
+                        && results[i].weight == 1 || results.weight == 2 ? results[i].value - 1 :  results[i].value;
+                    }
+                });
     }]);
 })();
