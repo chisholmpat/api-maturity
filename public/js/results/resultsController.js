8581bff8a19d1ee8f33b11667fc23cdaed9cfe1e
rosejp <rose@ca.ibm.com>
2015-12-13 00:34:54 -0400

    Updated averaging and using averages to calculate the graph input values

--- public/js/results/resultsController.js
+++ public/js/results/resultsController.js
@@ -13,24 +13,66 @@
       }
     }
 
+    function getAverages(groupedup_array){
+      var i,j;
+      var internalArrayLength = 0;
+      var maxGraphPoints = 5;
+      var total;
+      var averages_array = [];
+      for(i=0;i<maxGraphPoints;i++){
+        internalArrayLength = groupedup_array[i].length;
+        total =0;
+        for(j=0;j<internalArrayLength;j++){
+          total+=groupedup_array[i][j];
+        }
+        averages_array.push(total/(j+1));
+      }
+      return averages_array;
+    }
+
+    function getGraphInputs(averages_array){
+      var graphInputsArray = [];
+      var array_length = averages_array.length;
+      var i;
+
+      for(i=0; i<array_length; i++){
+        if(averages_array[i] <1.1){
+            graphInputsArray[i] =0;
+        }
+        else if(1<averages_array[i]<2.5){
+            graphInputsArray[i] = 1;
+        }
+        else{
+            graphInputsArray[i] = 2;
+        }
+      }
+      return graphInputsArray;
+    }
+
+
     function getGraphScores(results){
 
       var array_length = results.length;
       var groupedup_array_one = [[],[],[],[],[]];
-      var groupedup_array_two = [[],[],[],[],[]];
+      var averages_array_one =[];
+      var graph_input_one =[];
+      var graph_input_two =[];
       var group_name;
 
       //Calculate Scores
       for(i=0; i<array_length; i++){
           results[i].score = calculateAllScores(results[i].value, results[i].weight);
-          if(results[i].category_id ==1){
+          if(results[i].category_id ==1){//Consider changing to category_name == "QA"
             groupedup_array_one[results[i].group_id -1].push(results[i].score);
           }
           else {
-            groupedup_array_two[results[i].group_id -1].push(results[i].score);
+            graph_input_two.push(results[i].value);
           }
       }
-      //get the average for each group sharing a group_id
+      console.log(groupedup_array_one);
+      //get the average for each group sharing a group_id and convert to graphInput for category_id =1
+      averages_array_one = getAverages(groupedup_array_one);
+      graph_input_one = getGraphInputs(averages_array_one);
 
 
     }
