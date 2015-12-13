bc0ca376aea8bf424696db7412f7bda5fc30493b
rosejp <rose@ca.ibm.com>
2015-12-13 16:01:51 -0400

    Graph input arrays updated to be hast maps

--- public/js/results/resultsController.js
+++ public/js/results/resultsController.js
@@ -16,36 +16,35 @@
     }
 
     function getAverages(groupedup_array){
-      var i,j;
+      var key,j;
       var internalArrayLength = 0;
       var maxGraphPoints = 5;
       var total;
-      var averages_array = [];
-      for(i=0;i<maxGraphPoints;i++){
-        internalArrayLength = groupedup_array[i].length;
+      var averages_array = {};
+      for(key in groupedup_array){
+        internalArrayLength = groupedup_array[key].length;
         total =0;
         for(j=0;j<internalArrayLength;j++){
-          total+=groupedup_array[i][j];
+          total+=groupedup_array[key][j];
         }
-        averages_array.push(total/(j+1));
+        averages_array[key] = (total/(j+1));
       }
       return averages_array;
     }
 
     function getGraphInputs(averages_array){
-      var graphInputsArray = [];
-      var array_length = averages_array.length;
-      var i;
+      var graphInputsArray = {};
+      var key;
 
-      for(i=0; i<array_length; i++){
-        if(averages_array[i] <1.1){
-            graphInputsArray[i] =0;
+      for(key in averages_array ){
+        if(averages_array[key] <1.1){
+            graphInputsArray[key] =0;
         }
         else if(1<averages_array[i]<2.5){
-            graphInputsArray[i] = 1;
+            graphInputsArray[key] = 1;
         }
         else{
-            graphInputsArray[i] = 2;
+            graphInputsArray[key] = 2;
         }
       }
       return graphInputsArray;
@@ -55,20 +54,23 @@
     function getGraphScores(results){
 
       var array_length = results.length;
-      var groupedup_array_one = [[],[],[],[],[]];
-      var averages_array_one =[];
-      var graph_input_one =[];
-      var graph_input_two =[];
+      var groupedup_array_one = {};
+      var averages_array_one ={};
+      var graph_input_one ={};
+      var graph_input_two ={};
 
 
       //Calculate Scores
       for(i=0; i<array_length; i++){
           results[i].score = calculateAllScores(results[i].value, results[i].weight);
           if(results[i].category_id ==1){//Consider changing to category_name == "QA"
-            groupedup_array_one[results[i].group_id -1].push(results[i].score);
+            if(!groupedup_array_one[results[i].name]){//making a hash-map, "gropuname": [array of numbers to be averaged]
+              groupedup_array_one[results[i].name] = [];
+            }
+            groupedup_array_one[results[i].name].push(results[i].score);
           }
           else {
-            graph_input_two.push(results[i].value);
+            graph_input_two[results[i].name] = results[i].value;
           }
       }
       console.log(groupedup_array_one);
