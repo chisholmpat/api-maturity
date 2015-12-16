4f9166a461d01ea232da8bb17e1d3059be955694
rosejp <rose@ca.ibm.com>
2015-12-15 23:14:11 -0400

    Login code with passport and querying

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -1,3 +1,12 @@
+// Call back function from the database call. Used to send either the results
+// to the response or to send an error string to the response.
+// Dependencies
+var mysql = require('mysql');
+var queries = require('../querying/usersQuerying');
+
+
+
+
 module.exports = function (app, passport) {
 
 
@@ -5,12 +14,29 @@ module.exports = function (app, passport) {
 
   passport.use('local-login', new LocalStrategy(
     function(username, password, cb) {
-      return cb(null, false);
+
+      var queryReturn;
+      console.log("Reached the stratergy");
+      // console.log("Done with querying");
+      queryReturn = queries.userLoginValidate(username, password, "doesn'tmatter", function (res, err_string, results_array) {
+          if (!err_string) {
+              if(results_array.length != 0){
+                console.log("Valid User");
+                cb(null, true);
+              }
+              else{
+                console.log("user-invalid");
+                cb(null, false);
+              }
+          }
+          else {
+              console.log(err_string);
+              cb(null, false);
+          }
+      });
     }
   ));
 
-  app.post('/userLogin',
-    passport.authenticate('local-login', { session: false, failureRedirect: '/#/userLogin', successRedirect: '/'  } )
-  );
+  app.post('/userLogin',passport.authenticate('local-login', { session:false, failureRedirect: '/#/userLogin', successRedirect: '/' }));
 
 };
