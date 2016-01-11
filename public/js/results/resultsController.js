0e3b794293d90d5b6a1a3f4dcddd85c07b014089
Gregory Hatt <groggle@yandex.com>
2016-01-11 11:09:15 -0400

    Add CSV Export Option in Results Page.

--- public/js/results/resultsController.js
+++ public/js/results/resultsController.js
@@ -1,178 +1,177 @@
-
-
 (function() {
 
 
-	  // Retrieve all information required to generate score.
+    // Retrieve all information required to generate score.
 
-    function calculateAllScores(response_value, question_weight){
+    function calculateAllScores(response_value, question_weight) {
 
-      if( (response_value==1||response_value==2) && (question_weight==1||question_weight==2) ){
-        return response_value-1;
-      }
-      else{
-        return response_value;
-      }
+        if ((response_value == 1 || response_value == 2) && (question_weight == 1 || question_weight == 2)) {
+            return response_value - 1;
+        } else {
+            return response_value;
+        }
     }
 
-    function getAverages(groupedup_array){
-      var key,j;
-      var internalArrayLength = 0;
-      var maxGraphPoints = 5;
-      var total;
-      var averages_array = {};
+    function getAverages(groupedup_array) {
+        var key, j;
+        var internalArrayLength = 0;
+        var maxGraphPoints = 5;
+        var total;
+        var averages_array = {};
 
-      for(key in groupedup_array){
-        internalArrayLength = groupedup_array[key].length;
-        total =0;
-        for(j=0;j<internalArrayLength;j++){
-          total+=groupedup_array[key][j];
-        }
-        averages_array[key] = (total/(j+1));
-      }
-      return averages_array;
-    }
-
-    function getGraphInputs(averages_array){
-      var graphInputsArray = {};
-      var key;
-
-      for(key in averages_array ){
-        if(averages_array[key] <1.1){
-            graphInputsArray[key] =0;
-        }
-        else if(1<averages_array[i]<2.5){
-            graphInputsArray[key] = 1;
-        }
-        else{
-            graphInputsArray[key] = 2;
-        }
-      }
-      return graphInputsArray;
-    }
-
-
-    function getGraphScores(results){
-
-      var array_length = results.length;
-      var groupedup_array_one = {};
-      var averages_array_one ={};
-      var graph_input_one ={};
-      var graph_input_two ={};
-
-
-      //Calculate Scores
-      for(i=0; i<array_length; i++){
-          results[i].score = calculateAllScores(results[i].value, results[i].weight);
-          if(results[i].category_id ==1){//Consider changing to category_name == "QA"
-            if(!groupedup_array_one[results[i].name]){//making a hash-map, "gropuname": [array of numbers to be averaged]
-              groupedup_array_one[results[i].name] = [];
+        for (key in groupedup_array) {
+            internalArrayLength = groupedup_array[key].length;
+            total = 0;
+            for (j = 0; j < internalArrayLength; j++) {
+                total += groupedup_array[key][j];
             }
-            groupedup_array_one[results[i].name].push(results[i].score);
-          }
-          else {
-            graph_input_two[results[i].name] = results[i].value;
-          }
-      }
-      //get the average for each group sharing a group_id and convert to graphInput for category_id =1
-      averages_array_one = getAverages(groupedup_array_one);
-      graph_input_one = getGraphInputs(averages_array_one);
+            averages_array[key] = (total / (j + 1));
+        }
+        return averages_array;
+    }
 
-      results.graphingArrayCategoryOne = graph_input_one;//QA's graph input
-      results.graphingArrayCategoryTwo = graph_input_two; //SA'sgraph inpu
+    function getGraphInputs(averages_array) {
+        var graphInputsArray = {};
+        var key;
+
+        for (key in averages_array) {
+            if (averages_array[key] < 1.1) {
+                graphInputsArray[key] = 0;
+            } else if (1 < averages_array[i] < 2.5) {
+                graphInputsArray[key] = 1;
+            } else {
+                graphInputsArray[key] = 2;
+            }
+        }
+        return graphInputsArray;
+    }
+
+
+    function getGraphScores(results) {
+
+        var array_length = results.length;
+        var groupedup_array_one = {};
+        var averages_array_one = {};
+        var graph_input_one = {};
+        var graph_input_two = {};
+
+
+        //Calculate Scores
+        for (i = 0; i < array_length; i++) {
+            results[i].score = calculateAllScores(results[i].value, results[i].weight);
+            if (results[i].category_id == 1) { //Consider changing to category_name == "QA"
+                if (!groupedup_array_one[results[i].name]) { //making a hash-map, "gropuname": [array of numbers to be averaged]
+                    groupedup_array_one[results[i].name] = [];
+                }
+                groupedup_array_one[results[i].name].push(results[i].score);
+            } else {
+                graph_input_two[results[i].name] = results[i].value;
+            }
+        }
+        //get the average for each group sharing a group_id and convert to graphInput for category_id =1
+        averages_array_one = getAverages(groupedup_array_one);
+        graph_input_one = getGraphInputs(averages_array_one);
+
+        results.graphingArrayCategoryOne = graph_input_one; //QA's graph input
+        results.graphingArrayCategoryTwo = graph_input_two; //SA'sgraph inpu
 
     }
 
 
-    function makeRadarGraphs(graph_array, showGraph, hideGraph){
+    function makeRadarGraphs(graph_array, showGraph, hideGraph) {
 
-      var keyArray = [];
-      var dataArray = [];
-      var key;
-      var showGraphId;
-      var hideGraphId;
+        var keyArray = [];
+        var dataArray = [];
+        var key;
+        var showGraphId;
+        var hideGraphId;
 
-      for(key in graph_array){
-        keyArray.push(key);
-        dataArray.push(graph_array[key]);
-      }
+        for (key in graph_array) {
+            keyArray.push(key);
+            dataArray.push(graph_array[key]);
+        }
 
-      // Radar chart data
-      var graphData = {
-          labels : keyArray,
-          datasets : [
-          {
-              label: showGraph,
-              fillColor : "rgba(172,194,132,0.4)",
-              strokeColor : "#ACC26D",
-              pointColor : "#fff",
-              pointStrokeColor : "#9DB86D",
-              data : dataArray
-          }
-          ]
-      }
+        // Radar chart data
+        var graphData = {
+            labels: keyArray,
+            datasets: [{
+                label: showGraph,
+                fillColor: "rgba(172,194,132,0.4)",
+                strokeColor: "#ACC26D",
+                pointColor: "#fff",
+                pointStrokeColor: "#9DB86D",
+                data: dataArray
+            }]
+        }
 
-      showGraphId = document.getElementById(showGraph);
-      hideGraphId = document.getElementById(hideGraph);
-      // get radar chart canvas
-      var radarChart = document.getElementById(showGraph).getContext('2d');
+        showGraphId = document.getElementById(showGraph);
+        hideGraphId = document.getElementById(hideGraph);
+        // get radar chart canvas
+        var radarChart = document.getElementById(showGraph).getContext('2d');
 
-      // draw radar chart
-      new Chart(radarChart).Radar(graphData);
+        // draw radar chart
+        new Chart(radarChart).Radar(graphData);
 
 
-      // //hide the other canvas
-      // hideGraphId.style.display = "none";
-      // showGraphId.style.display = "block";
+        // //hide the other canvas
+        // hideGraphId.style.display = "none";
+        // showGraphId.style.display = "block";
 
     }
 
-    function makeGaugeGraphs(graph_array, showGraph, hideGraph){
+    function makeGaugeGraphs(graph_array, showGraph, hideGraph) {
 
-      var graphData =[];
-      var showGraphId;
-      var hideGraphId;
-      var max_val=2;
-      var key;
+        var graphData = [];
+        var showGraphId;
+        var hideGraphId;
+        var max_val = 2;
+        var key;
 
-      graphData.push(['Label', 'Value']);
-      for(key in graph_array){
-        graphData.push([key, (graph_array[key]/max_val)*100 ]);//converting the value to a percentage
-      }
+        graphData.push(['Label', 'Value']);
+        for (key in graph_array) {
+            graphData.push([key, (graph_array[key] / max_val) * 100]); //converting the value to a percentage
+        }
 
-      var data = google.visualization.arrayToDataTable(graphData);
+        var data = google.visualization.arrayToDataTable(graphData);
 
-      var options = {
-        width: 400, height: 120,
-        redFrom: 90, redTo: 100,
-        yellowFrom:75, yellowTo: 90,
-        minorTicks: 5
-      };
+        var options = {
+            width: 400,
+            height: 120,
+            redFrom: 90,
+            redTo: 100,
+            yellowFrom: 75,
+            yellowTo: 90,
+            minorTicks: 5
+        };
 
-      var chart = new google.visualization.Gauge(document.getElementById(showGraph));
+        var chart = new google.visualization.Gauge(document.getElementById(showGraph));
 
-       chart.draw(data, options);
+        chart.draw(data, options);
 
     }
 
     var module = angular.module('resultsModule', ['resultsServiceModule']);
-    module.controller('ResultsController', ['$scope', '$routeParams', 'ResultStore', function($scope, $routeParams, ResultStore) {
+    module.controller('ResultsController', ['$scope', '$routeParams', 'ResultStore',
+        function($scope, $routeParams, ResultStore) {
 
-    $scope.results = ResultStore.scoreConn.query({
-            client_id: $routeParams.client_id,
-            form_id: $routeParams.form_id
-          },function(){
-              graphingArray = getGraphScores($scope.results);
-              $scope.graphingFunction= function(){
-                function drawCharts() {
-                      makeRadarGraphs($scope.results.graphingArrayCategoryOne, 'QAgraph', 'SAgraph');
-                      makeRadarGraphs($scope.results.graphingArrayCategoryTwo, 'SAgraph', 'QAgraph');
-                      makeGaugeGraphs($scope.results.graphingArrayCategoryOne, 'QAgaugeGraph', 'SAgaugeGraph');
-                      makeGaugeGraphs($scope.results.graphingArrayCategoryTwo, 'SAgaugeGraph', 'QAgaugeGraph');
+            // URL for retrieving results as a CSV file
+            $scope.csvURL = "questions/" + $routeParams.client_id + "/" + $routeParams.form_id + "/csv"
+
+            $scope.results = ResultStore.scoreConn.query({
+                client_id: $routeParams.client_id,
+                form_id: $routeParams.form_id
+            }, function() {
+                graphingArray = getGraphScores($scope.results);
+                $scope.graphingFunction = function() {
+                    function drawCharts() {
+                        makeRadarGraphs($scope.results.graphingArrayCategoryOne, 'QAgraph', 'SAgraph');
+                        makeRadarGraphs($scope.results.graphingArrayCategoryTwo, 'SAgraph', 'QAgraph');
+                        makeGaugeGraphs($scope.results.graphingArrayCategoryOne, 'QAgaugeGraph', 'SAgaugeGraph');
+                        makeGaugeGraphs($scope.results.graphingArrayCategoryTwo, 'SAgaugeGraph', 'QAgaugeGraph');
+                    }
+                    google.charts.setOnLoadCallback(drawCharts); //Only once charts loaded drawing charts is executed, this takes care of page refresh
                 }
-                google.charts.setOnLoadCallback(drawCharts);//Only once charts loaded drawing charts is executed, this takes care of page refresh
-              }
-          }
-        );
-    }]);
+            });
+        }
+    ]);
 })();
--- routes/questions.js
+++ routes/questions.js
@@ -3,58 +3,80 @@
 function callback(res, err_string, results_array) {
     if (!err_string) {
         res.send(results_array);
-    }
-    else {
+    } else {
         console.log(err_string);
     }
 }
 
-module.exports = function (app) {
+module.exports = function(app) {
+
     // Dependencies
     var queries = require('../querying/questionsQuerying');
+    var csv = require('express-csv');
 
     // Return all questions by client and form id
-    app.get('/questions/:client_id/:form_id', function (req, res) {
+    app.get('/questions/:client_id/:form_id', function(req, res) {
         queries.getAllQuestions(req.params.client_id, req.params.form_id, res, callback);
     });
 
+    // This is just temperorary until I'm able to figure out how to make it 
+    // so that the request will be formatted as /question/1/2.csv
+    app.get('/questions/:client_id/:form_id/csv', function(req, res) {
+
+        var sendCSV = function(res, err_string, results) {
+            var headers = {};
+            for (key in results[0]) {
+                headers[key] = key;
+            }
+            
+            var fileName = req.params.client_id + '_' + req.params.form_id + '.csv';
+            res.setHeader('Content-disposition', 'attachment; filename=' +fileName)
+            results.unshift(headers);
+            res.csv(results);
+        }
+
+        queries.getClientAnswers(req.params.client_id, req.params.form_id, res, sendCSV);
+
+    });
+
+
     // Return all scores
-    app.get('/score/:client_id/:form_id', function (req, res) {
+    app.get('/score/:client_id/:form_id', function(req, res) {
         queries.getClientAnswers(req.params.client_id, req.params.form_id, res, callback);
     });
 
     // Get all possible responses
-    app.get('/responses', function (req, res) {
+    app.get('/responses', function(req, res) {
         queries.getAllResponses(res, callback);
     });
 
     // Get all forms for a particular client
-    app.get('/forms/:client_id', function (req, res) {
+    app.get('/forms/:client_id', function(req, res) {
         queries.getAllFormsByClient(req.params.client_id, res, callback);
     });
 
     // Get a list of all the clients
-    app.get('/clients', function (req, res) {
+    app.get('/clients', function(req, res) {
         queries.getClients(res, callback);
     });
 
     // Get a list of all the forms.
-    app.get('/forms', function (req, res) {
+    app.get('/forms', function(req, res) {
         queries.getAllForms(res, callback);
     });
 
     // Get the questions associated with a form.
-    app.get("/questions/:form_id", function (req, res) {
-	queries.getQuestionsByForm(req.params.form_id, res, callback);
+    app.get("/questions/:form_id", function(req, res) {
+        queries.getQuestionsByForm(req.params.form_id, res, callback);
     });
 
     //
-    app.get("/groupings", function (req, res) {
+    app.get("/groupings", function(req, res) {
         queries.getGroupings(res, callback);
     });
 
     // Update questions
-    app.post("/update_questions", function (req, res) {
+    app.post("/update_questions", function(req, res) {
         queries.updateQuestions(res, callback, req.body.questions);
     });
 
