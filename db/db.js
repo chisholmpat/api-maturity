f96321959443b27a867f77f6ca8a007843e2928e
Gregory Hatt <groggle@yandex.com>
2016-01-13 16:43:21 -0400

    Database Power!

--- db/db.js
+++ db/db.js
@@ -1,32 +1,14 @@
 var opts = null;
 
-if (process.env.VCAP_SERVICES) {
-    var services = JSON.parse(process.env.VCAP_SERVICES);
-
-    for (var svcName in services) {
-        if (svcName.match(/^cleardb/)) {
-            var mysqlCreds = services[svcName][0]['credentials'];
-            opts = {
-                host: mysqlCreds.host,
-                host: mysqlCreds.hostname,
-                port: mysqlCreds.port,
-                user: mysqlCreds.username,
-                password: mysqlCreds.password,
-                database: mysqlCreds.name,
-                multipleStatements: true
-            }
-        }
-    }
-} else {
     opts = {
-        host: 'us-cdbr-iron-east-03.cleardb.net',
-        user: 'bbcabd11525ed5',
-        password: '102fb0ce',
-        database: 'ad_a9e53e0371a808b',
+        host: 'us-cdbr-azure-southcentral-e.cloudapp.net',
+        user: 'b8f6c834853270',
+        password: 'd5252709',
+        database: 'API-Maturity',
         multipleStatements: true,
         connectionLimit: 1
     }
-}
+
 
 exports.knex = require('knex')({
     client: 'mysql',
