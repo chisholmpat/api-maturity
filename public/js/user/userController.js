17f0daaca53117ade646c91fa70e1e022cfd6b77
Gregory Hatt <groggle@yandex.com>
2016-01-27 22:24:48 -0400

    Add comments to controllers and HTML page for Users.

--- public/js/user/userController.js
+++ public/js/user/userController.js
@@ -2,18 +2,19 @@
 
     var module = angular.module('userModule', ['userServiceModule']);
 
+    // Controller for login operations, both SSO and database backed logins.
     module.controller('UserLoginController', function($scope, $rootScope, $http, $location, $window) {
-        
-		// To
-		$rootScope.$watch('isLoggedIn', function() {
+
+        // Redirect the location if already logged in.
+        $rootScope.$watch('isLoggedIn', function() {
             if ($rootScope.isLoggedIn)
                 $window.top.location = $window.location;
         });
-		
-		// This object will be filled by the form
+
+        // This object will be filled by the form
         $scope.user = {};
 
-        // Register the login() function
+        // The login function
         $scope.login = function() {
             $http.post('/login', {
                     username: $scope.user.username,
@@ -39,25 +40,25 @@
             $scope.result = ""; // 
             $scope.editing = {};
             $scope.defaultRoleID = "";
-			
-			// Get a list of all the available users
-			$scope.allUsers = UserStore.getUsers.query();
-			
-			// Retrieve roles for use in the dropdown
+
+            // Get a list of all the available users
+            $scope.allUsers = UserStore.getUsers.query();
+
+            // Retrieve roles for use in the dropdown
             $scope.roles = UserStore.getUserRoles.query({}, function(results) {
                 $scope.defaultRoleID = results[0].id;
                 $scope.editing.role_id = $scope.defaultRoleID
             });
 
-			// Ensure that the password is appropriate.
+            // Ensure that the password is appropriate.
             $scope.handlePatternPassword = (function() {
                 var regex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%]).{6,20})/;
                 return {
                     test: function(value) {
-                        if (!$scope.editing.id) 
+                        if (!$scope.editing.id)
                             return (value.length > 0) ? regex.test(value) : true;
-                         else 
-                            return true;      
+                        else
+                            return true;
                     }
                 };
             })();
@@ -83,7 +84,6 @@
             // it will add it.
             $scope.submit = function(newUser) {
 
-
                 if ($scope.aUser) {
                     // Handle Updating Client
                     UserStore.updateUser.save({
@@ -98,11 +98,13 @@
                         user: $scope.editing
                     }, function() {
                         $scope.result = "User Added";
-                        $scope.allUsers = UserStore.getUsers.query();
+					    // Clear the form
                         $scope.editing = {};
                         $scope.editing.role_id = $scope.defaultRoleID
+						$scope.passwordConfirm = "";
                         $scope.userform.$setPristine();
-                        $scope.passwordConfirm = "";
+						// Refresh from the database
+						$scope.allUsers = UserStore.getUsers.query(); 
 
                     });
                 }
@@ -111,48 +113,49 @@
         }
     ]);
 
-
-// Controller for handling the actual resetting of the password.
-module.controller('PasswordResetController', ['$scope', 'UserStore', '$location', '$routeParams',
+    // Controller for handling the actual resetting of the password.
+    module.controller('PasswordResetController', ['$scope', 'UserStore', '$location', '$routeParams',
         function($scope, UserStore, $location, $routeParams) {
 
+			// Checks to see if a token for password reset
+			// actually exists in the user table. Used to avoid
+			// wrong token addresses or old tokens.
             UserStore.checkToken.query({
-                token : $routeParams.token
+                token: $routeParams.token
             }, function(response) {
-                    console.log('TOKEN RECOGNIZED');
-            }, function(){
-                   console.log('TOKEN NOT RECOGNIZED')
-                   $location.url("/#/");
+            }, function() {
+                $location.url("/#/");
             });
 
-            $scope.submit = function(){
+			// Used to update the password in the database.
+            $scope.submit = function() {
                 UserStore.updatePassword.save({
-                  token: $routeParams.token,
-                  password: $scope.pw1,
-                }, function(){
-					$location.url("/#/");
-				})
+                    token: $routeParams.token,
+                    password: $scope.pw1,
+                }, function() {
+                    $location.url("/#/");
+                })
             }
-        }]);
-		
-// Module responsible for sending the email to the registed user's email account.
-module.controller('SendPasswordController', ['$scope', 'UserStore', '$location', '$routeParams', '$window',
-	function($scope, UserStore, $location, $routeParams, $window) {
-		
-			$scope.submit = function() {
-				UserStore.sendPasswordEmail.save({
-					email: $scope.email
-				}, function(){
-					 window.alert("Email send to " + $scope.email);
-					 $location.url("/#/")
-					 
-				}, function(){
-					 $scope.emailMessage = "Email not recognized."	 
-				})
-			}
-	}]);
+        }
+    ]);
 
-   module.controller('EditUserController', function($scope, $rootScope, $location, $window) {
+    // Controller responsible for sending the email to the registed user's email account.
+    module.controller('SendPasswordController', ['$scope', 'UserStore', '$location', '$routeParams', '$window',
+        function($scope, UserStore, $location, $routeParams, $window) {
 
-    });
+            $scope.submit = function() {
+                UserStore.sendPasswordEmail.save({
+                    email: $scope.email
+                }, function() {
+                    window.alert("Email send to " + $scope.email);
+                    $location.url("/#/")
+
+                }, function() {
+                    $scope.emailMessage = "Email not recognized."
+                })
+            }
+        }
+    ]);
+
+
 })();
